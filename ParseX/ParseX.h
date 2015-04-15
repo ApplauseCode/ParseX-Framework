9082cb181701571a302eb868373e018d3df74b9a
jeffreyrosenbluth <jeffrey.rosenbluth@gmail.com>
2015-04-14 20:24:16 -0400

    Initial Commit

---
+++ ParseX/ParseX.h
@@ -0,0 +1,19 @@
+//
+//  ParseX.h
+//  ParseX
+//
+//  Created by Jeffrey Rosenbluth on 4/14/15.
+//  Copyright (c) 2015 Jeffrey Rosenbluth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParseX.
+FOUNDATION_EXPORT double ParseXVersionNumber;
+
+//! Project version string for ParseX.
+FOUNDATION_EXPORT const unsigned char ParseXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParseX/PublicHeader.h>
+
+
